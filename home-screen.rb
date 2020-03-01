9194f761d13d47d93c61806e28fd31d678c510e6
giftTheProgrammer <m.g.moletsane@gmail.com>
2020-03-01 21:04:44 +0200

    added home-screen.rb

---
+++ home-screen.rb
@@ -0,0 +1,26 @@
+CATEGORY1 = "Food"
+CATEGORY2 = "Transport"
+CATEGORY3 = "Airtime"
+CATEGORY4 = "Toiletries"
+CATEGORY5 = "Home"
+
+puts "MENU \n"
+puts "1 = " + CATEGORY1
+puts "2 = " + CATEGORY2
+puts "3 = " + CATEGORY3
+puts "4 = " + CATEGORY4
+puts "5 = " + CATEGORY5 + "\n"
+
+puts "Enter your choice"
+c = gets.to_i
+
+case c
+when c == 1
+  puts "How much did you spend on " + CATEGORY1 + "?"
+when c == 2
+  puts "How much did you spend on " + CATEGORY2 + "?"
+
+end
+when condition
+
+end
